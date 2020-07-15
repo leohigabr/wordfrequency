6053a0fc16c8ab44681ad7f2db0f5e07a2134638
Leonardo Higa <leonardiga@gmail.com>
2020-07-15 01:40:37 -0300

    Basic frontend implemented. Trying to clean the input properly.

--- main.js
+++ main.js
@@ -1,2 +1,31 @@
-const words = document.querySelector('.inputBox').value.split(" ");
-console.log(words);
+const checkBtn = document.getElementById('countBtn');
+
+const getInput = () => {
+     return (document.querySelector('.inputBox')
+        .value
+        .replace(/[^\w\s]/gi, '')
+        .toLowerCase()
+        .trim()
+        .split(' ')
+     )
+}
+
+checkBtn.addEventListener('click', () => {
+    const words = getInput();
+    console.log(words);
+    console.log("length:", words.length);
+
+    /* I will continue this code below, after
+       discovering a way of cleaning (properly) and
+       removing line breaks from the input.
+
+    let count = [];
+    for(let i=0;i<words.length;i++){
+        for(let j=0;j<words.length;j++){
+            if(words[i] == words[j]){
+                count++;
+            }
+        }
+    }
+    */
+})
